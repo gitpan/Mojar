7f9669ec4c6dd9f00e92131ca2a4a118377c9d68
Nic Sandfield <niczero@cpan.org>
2014-02-11 23:13:45 +0000

    Import of NICZERO/Mojar-1.062 from CPAN.
    gitpan-cpan-distribution: Mojar
    gitpan-cpan-version:      1.062
    gitpan-cpan-path:         NICZERO/Mojar-1.062.tar.gz
    gitpan-cpan-author:       NICZERO
    gitpan-cpan-maturity:     released

--- lib/Mojar.pm
+++ lib/Mojar.pm
@@ -1,9 +1,9 @@
 package Mojar;
 use Mojo::Base -strict;
 
-our $VERSION = 0.017;
+our $VERSION = 1.062;
 
-1
+1;
 __END__
 
 =head1 NAME
@@ -12,16 +12,16 @@ Mojar - Integration toolkit
 
 =head1 SYNOPSIS
 
-  use Mojar::Util q(detitlecase titlecase);
+  use Mojar::Util qw(snakecase unsnakecase);
   use Mojar::Cache;
+  use Mojar::Log (pattern => '%R');
 
 =head1 DESCRIPTION
 
 A bag of tools for integrating to various APIs.  Most of the tools are provided
 in separate distributions to continue the theme of keeping your footprint small.
-All rely on having a fairly recent release of Mojolicious installed.
 
-=head2 DISTRIBUTIONS
+=head1 DISTRIBUTIONS
 
 =over 4
 
@@ -30,17 +30,12 @@ All rely on having a fairly recent release of Mojolicious installed.
 Includes easy connection management, replication monitoring, and schema
 analysis.
 
-=item Mojar::Google::Analytics
-
-Supports unattended logins and data extraction for reporting.
-
-=item Mojar::Cron
-
+=for comment
+item Mojar::Google::Analytics
+Draw down your web analytics data for reporting.
+item Mojar::Cron
 Calculate when the next instance should run.
-
-=item Mojar::BulkSms
-
-Send (unattended) SMS via the net service.
+=end comment
 
 =back
 
@@ -48,30 +43,27 @@ Send (unattended) SMS via the net service.
 
 =head2 IRC
 
-C<#mojo> on C<irc.perl.org>
+C<nic> at C<#mojo> on C<irc.perl.org>
 
-=head2 WIKI
+=head2 Wiki
 
 L<https://github.com/niczero/mojar/wiki>
 
-=head1 DEVELOPMENT
-
-=head2 Repository
-
-L<http://github.com/niczero/mojar>
-
 =head1 RATIONALE
 
-Mojolicious is an awesome web application framework that just so happens to be
-the foundation of a fabulous web integration platform.  The intention of Mojar
-is to provide pluggable classes that extend the same philosophies while getting
+Mojolicious is an awesome web application framework that includes many great
+building blocks even for non-web development and integration.  The intention of
+Mojar is to provide pluggable classes that extend that approach while getting
 you closer to connecting to third-party services.
 
 =head1 COPYRIGHT AND LICENCE
 
-Copyright (C) 2012, Nic Sandfield.
+Copyright (C) 2008--2012, Sebastian Riedel.
+
+Copyright (C) 2012--2014, Nic Sandfield.
 
 This program is free software, you can redistribute it and/or modify it under
 the terms of the Artistic License version 2.0.
 
-=cut
+All of this code is inspired by Mojolicious and the great work of Sebastian
+Riedel.  In particular Config is a direct fork of its Mojolicious counterpart.
